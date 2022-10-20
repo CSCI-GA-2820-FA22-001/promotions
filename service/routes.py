c0328ca806193f9052bda45e1e03cfd6368e9306
Joseph Salerno <56265826+JosephSalerno@users.noreply.github.com>
2022-10-20 03:45:28 +0000

    added skeleton and create,update, and delete

--- service/routes.py
+++ service/routes.py
@@ -1,15 +1,13 @@
 """
-My Service
+Promotion Service
 
-Describe what your service does here
+This microservice handles the lifecycle of Promotions
 """
 
-from flask import Flask, jsonify, request, url_for, make_response, abort
-from .common import status  # HTTP Status Codes
+from flask import jsonify, request, url_for, make_response, abort
 from service.models import Promotion
-
-# Import Flask application
-from . import app
+from service.common import status  # HTTP Status Codes
+from . import app  # Import Flask application
 
 
 ######################################################################
@@ -17,12 +15,15 @@ from . import app
 ######################################################################
 @app.route("/")
 def index():
-    """ Root URL response """
+    """Root URL response"""
     return (
-        "Reminder: return some useful information in json format about the service here",
+        jsonify(
+            name="Promotion REST API Service",
+            version="1.0",
+            paths=url_for("list_promotions", _external=True),
+        ),
         status.HTTP_200_OK,
     )
-
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
@@ -33,3 +34,107 @@ def init_db():
     """ Initializes the SQLAlchemy app """
     global app
     Promotion.init_db(app)
+
+def check_content_type(media_type):
+    """Checks that the media type is correct"""
+    content_type = request.headers.get("Content-Type")
+    if content_type and content_type == media_type:
+        return
+    app.logger.error("Invalid Content-Type: %s", content_type)
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        f"Content-Type must be {media_type}",
+    )
+
+
+######################################################################
+# LIST ALL Promotions
+######################################################################
+@app.route("/promotions", methods=["GET"])
+def list_promotions():
+    """Returns all of the Promotions"""
+    pass
+
+
+######################################################################
+# RETRIEVE A Promotion
+######################################################################
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_promotion(account_id):
+    """
+    Retrieve a single Promotion
+    This endpoint will return an Promotion based on it's id
+    """
+    pass
+
+
+######################################################################
+# CREATE A NEW Promotion
+######################################################################
+@app.route("/promotions", methods=["POST"])
+def create_promotion():
+    """
+    Creates a Promotion
+    This endpoint will create an Promotion based the data in the body that is posted
+    """
+    app.logger.info("Request to create a Promotion")
+    check_content_type("application/json")
+
+    # Create the account
+    promotion = Promotion()
+    promotion.deserialize(request.get_json())
+    promotion.create()
+
+    # Create a message to return
+    message = promotion.serialize()
+    location_url = url_for("get_promotion", promotion_id=promotion.id, _external=True)
+
+    return make_response(
+        jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+    )
+
+
+######################################################################
+# UPDATE AN EXISTING Promotion
+######################################################################
+@app.route("/promotions/<int:promotion_id>", methods=["PUT"])
+def update_promotion(promotion_id):
+    """
+    Update a Promotion
+    This endpoint will update an Promotion based the body that is posted
+    """
+    app.logger.info("Request to update account with id: %s", promotion_id)
+    check_content_type("application/json")
+
+    # See if the account exists and abort if it doesn't
+    promotion = Promotion.find(promotion_id)
+    if not promotion:
+        abort(
+            status.HTTP_404_NOT_FOUND, f"Account with id '{promotion_id}' was not found."
+        )
+
+    # Update from the json in the body of the request
+    promotion.deserialize(request.get_json())
+    promotion.id = promotion_id
+    promotion.update()
+
+    return make_response(jsonify(promotion.serialize()), status.HTTP_200_OK)
+
+
+######################################################################
+# DELETE AN Promotion
+######################################################################
+@app.route("/promotions/<int:promotion_id>", methods=["DELETE"])
+def delete_promotion(promotion_id):
+    """
+    Delete a Promotion
+    This endpoint will delete an Promotion based the id specified in the path
+    """
+    app.logger.info("Request to delete account with id: %s", promotion_id)
+
+    # Retrieve the account to delete and delete it if it exists
+    promotion = Promotion.find(promotion_id)
+    if promotion:
+        promotion.delete()
+
+    return make_response("", status.HTTP_204_NO_CONTENT)
