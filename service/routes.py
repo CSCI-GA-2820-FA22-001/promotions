1c1aaf9d3a08fc72ae4d1f465fa4936cd6da9113
Ajeeta Asthana <ajita.asthana@gmail.com>
2022-12-13 22:40:40 +0000

    cancel promo removal

--- service/routes.py
+++ service/routes.py
@@ -311,30 +311,6 @@ class DeactivateResource(Resource):
         return promotion.serialize(), status.HTTP_200_OK
 
 ######################################################################
-#  CANCEL A PROMOTION - /promotions/{id}/cancel
-######################################################################
-@api.route("/promotions/<int:promotion_id>/cancel")
-@api.param('promotion_id', 'The Promotion identifier')
-class PromotionCancellation(Resource):
-    @api.doc('cancel_promotions')
-    @api.response(404, 'Promotion not found')
-    @api.response(200, 'Promotion cancelled')
-    def post(self, promotion_id):
-        """
-        Cancels a single Promotion
-        This endpoint will cancel a Promotion based on it's id
-        """
-        app.logger.info("Request to cancel Promotion with id: %s", promotion_id)
-        promotion = Promotion.find(promotion_id)
-        if not promotion:
-            raise NotFound("Promotion with id '{}' was not found.".format(promotion_id))
-        promotion.end_date = datetime.now()
-        promotion.update()
-        app.logger.info("Promotion with ID [%s] cancelled", promotion_id)
-        return make_response("", status.HTTP_200_OK)
-
-
-######################################################################
 # Health check for Kube
 ######################################################################
 @app.route("/health", methods=["GET"])
